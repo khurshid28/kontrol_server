4cb0ff348b7afead8772b7b91a16812e57c69b2b
khurshid28 <khurshidi2827@gmail.com>
2024-08-01 11:53:40 +0500

    >

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ app.use(express.json({limit: '20mb'}),);
 app.use(express.urlencoded({extended:true,limit: '20mb',parameterLimit : 20}));
 app.use(morgan("dev"), cors(), rateLimit(),authMiddleware );
 
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
 // testing server
 app.get("/api", (req, res) => res.send("KONTROL.UZ  API"));
 
