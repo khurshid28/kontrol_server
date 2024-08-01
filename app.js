efeea83e77fbed0a00083e4baa6e4fb1103e9f1a
khurshid28 <khurshidi2827@gmail.com>
2024-08-01 12:12:40 +0500

    >

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(morgan("dev"), cors(), rateLimit(),authMiddleware );
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Accept, Authorization, Content-Length, X-Requested-With');
 
   if ('OPTIONS' === req.method) {
     res.send(200);
