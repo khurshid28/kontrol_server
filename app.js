6b1376c3490b5a1a9590d24007765999c1f2448d
Khurshid Ismoilov <95979088+khurshid28@users.noreply.github.com>
2024-08-21 21:28:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -52,10 +52,10 @@ app.use(function (req, res, next) {
   }
 });
 // testing server
-app.get("/api", (req, res) => res.send("KONTROL.UZ  API"));
+app.get("/", (req, res) => res.send("KONTROL.UZ  API"));
 
 // all routes
-app.use("/api/v1", router);
+app.use("/v1", router);
 
 // app.use(
 //   helmet({
